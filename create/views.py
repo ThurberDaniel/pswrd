f7ee34506f01497c96cc06179ffe522bc28e94a8
Daniel <phxburg@gmail.com>
2022-03-26 15:40:16 -0600

    update3

--- create/views.py
+++ create/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
-# Create your views here.
+
 
 def home(request):
-    return render(request, 'create/home.html', {'passwrd':'0987uyhjkiuy765ret6y78*&^%$'})
+    return render(request, 'create/home.html')
 def about(request):
     return render(request, 'create/about.html')
 
@@ -15,7 +15,7 @@ def password(request):
     if request.GET.get('uppercase'):
         lowCharacters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
     if request.GET.get('special'):
-        lowCharacters.extend(list('?>*&^%$#@!+},'))
+        lowCharacters.extend(list('?>*&^%$#@!+},==++{?~!!'))
     if request.GET.get('numbers'):
         lowCharacters.extend(list('1234567890'))
     length = int(request.GET.get('length', 7))
