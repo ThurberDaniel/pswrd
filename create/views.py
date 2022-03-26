772e46855e7bca34b396a5a35d184c7759f2ab76
Daniel <phxburg@gmail.com>
2022-03-26 14:03:26 -0600

    update1

--- create/views.py
+++ create/views.py
@@ -6,17 +6,18 @@ import random
 
 def home(request):
     return render(request, 'create/home.html', {'passwrd':'0987uyhjkiuy765ret6y78*&^%$'})
+def about(request):
+    return render(request, 'create/about.html')
+
 
 def password(request):
     lowCharacters = list('abcdefghijklmnopqrstuvwxyz')
-
     if request.GET.get('uppercase'):
         lowCharacters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
     if request.GET.get('special'):
         lowCharacters.extend(list('?>*&^%$#@!+},'))
     if request.GET.get('numbers'):
         lowCharacters.extend(list('1234567890'))
-
     length = int(request.GET.get('length', 7))
     pswrd = ''
     for z in range(length):
--- pswrd11/urls.py
+++ pswrd11/urls.py
@@ -19,4 +19,5 @@ from create import views
 urlpatterns = [
     path('',views.home, name= 'home'),
     path('password/',views.password),
+    path('about/',views.about, name= 'about'),
 ]
